a11337ebcf342f3a6b69c4787d4c288277ed893c
evertonmessias <everton.messias@gmail.com>
2019-12-04 09:53:55 -0300

    04-12-2019_09:53:55

--- js/contact_me.js
+++ js/contact_me.js
@@ -9,8 +9,7 @@ $(function() {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
       var name = $("input#name").val();
-      var email = $("input#email").val();
-      var phone = $("input#phone").val();
+      var email = $("input#email").val();      
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
@@ -23,8 +22,7 @@ $(function() {
         url: "././mail/contact_me.php",
         type: "POST",
         data: {
-          name: name,
-          phone: phone,
+          name: name,          
           email: email,
           message: message
         },
