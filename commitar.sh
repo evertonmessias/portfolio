abc91bd05a6badd92a6359660414df8269df91a3
evertonmessias <everton.messias@gmail.com>
2019-12-04 23:22:55 -0300

    04-12-2019_23:22:55

--- commitar.sh
+++ commitar.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#git config credential.helper store
+git config credential.helper store
 #find ./ -iname .gitignore -exec rm {} \;
 #echo "##### GITIGNORE EXCLUIDO ==> OK #####"
-#git pull #puchar
 git add .
 echo "##### GIT ADD ==> OK #####"
 data=`date +%d-%m-%Y_%H:%M:%S`
