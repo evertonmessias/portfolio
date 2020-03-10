d68d1b816ed18cd0de50c6d43b49ddf93c0a979d
evertonmessias <everton.messias@gmail.com>
2020-03-10 11:40:34 -0300

    10-03-2020_11:40:34

--- index.php
+++ index.php
@@ -442,7 +442,7 @@
             <ul class="listas">
               <?php
               $li = 17;
-              $lf = 31;
+              $lf = 32;
               $i = 1;
               $arquivo = fopen('testes/php/index.php', 'r');
               if ($arquivo == false) {
